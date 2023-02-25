b26a5408dccda319e8904e8cf21be23575a89dab
Manuel Palumbo <manuel.palumbo@email.com>
2023-02-25 23:18:28 +0100

    Fix: Missing suggestions

--- src/main/java/ml/empee/commandsManager/parsers/types/BoolParser.java
+++ src/main/java/ml/empee/commandsManager/parsers/types/BoolParser.java
@@ -6,11 +6,17 @@ import lombok.experimental.SuperBuilder;
 import ml.empee.commandsManager.parsers.DescriptionBuilder;
 import ml.empee.commandsManager.parsers.ParameterParser;
 import ml.empee.commandsManager.utils.helpers.Tuple;
+import org.bukkit.command.CommandSender;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SuperBuilder
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class BoolParser extends ParameterParser<Boolean> {
+
+  private static final List<String> SUGGESTIONS = Arrays.asList("true", "false");
 
   @Override
   public DescriptionBuilder getDescriptionBuilder() {
@@ -23,6 +29,11 @@ public class BoolParser extends ParameterParser<Boolean> {
   @Override
   public Boolean parse(int offset, String... args) {
     return Boolean.parseBoolean(args[offset]);
+  }
+
+  @Override
+  public List<String> buildSuggestions(CommandSender source, String arg) {
+    return SUGGESTIONS;
   }
 
   @Override
